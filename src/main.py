a2e4b03f916a3e3478785a305ef55d26b89c290e
DESKTOP-0FNBS57\Vilijan <monevvilijan@gmail.com>
2021-06-19 08:50:18 +0200

    updated teal version.

--- src/main.py
+++ src/main.py
@@ -7,15 +7,21 @@ main_dev_pk, main_dev_address = main_developer_credentials()
 application_initialization_service = AppInitializationService(app_creator_pk=main_dev_pk,
                                                               app_creator_address=main_dev_address,
                                                               asa_unit_name="wawa",
-                                                              asa_asset_name="wawa")
+                                                              asa_asset_name="wawa",
+                                                              teal_version=3)
 
 application_initialization_service.create_application()
+
 application_initialization_service.create_asa()
+
 application_initialization_service.setup_asa_delegate_smart_contract()
 application_initialization_service.deposit_fee_funds_to_asa_delegate_authority()
+
 application_initialization_service.change_asa_credentials()
+
 application_initialization_service.setup_algo_delegate_smart_contract()
 application_initialization_service.deposit_fee_funds_to_algo_delegate_authority()
+
 application_initialization_service.setup_app_delegates_authorities()
 
 print(f'app_id: {application_initialization_service.app_id} \n'
@@ -27,7 +33,8 @@ bidder_pk, bidder_address = get_developer_credentials(developer_id=1)
 
 app_interaction_service = AppInteractionService(app_id=application_initialization_service.app_id,
                                                 asa_id=application_initialization_service.asa_id,
-                                                current_owner_address=main_dev_address)
+                                                current_owner_address=main_dev_address,
+                                                teal_version=3)
 
 app_interaction_service.execute_bidding(bidder_name="wawa",
                                         bidder_private_key=bidder_pk,
